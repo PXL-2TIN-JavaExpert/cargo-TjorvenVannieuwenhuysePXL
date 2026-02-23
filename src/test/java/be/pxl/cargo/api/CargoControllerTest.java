3e9d5744077792786f97ed99da0733d88257c416
TjorvenVannieuwenhuysePXL <185420224+TjorvenVannieuwenhuysePXL@users.noreply.github.com>
2026-02-23 15:52:57 +0100

    add tests

--- src/test/java/be/pxl/cargo/api/CargoControllerTest.java
+++ src/test/java/be/pxl/cargo/api/CargoControllerTest.java
@@ -1,9 +1,51 @@
 package be.pxl.cargo.api;
 
+import be.pxl.cargo.api.request.CreateCargoRequest;
+import be.pxl.cargo.domain.Location;
+import be.pxl.cargo.service.CargoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CargoController.class)
 public class CargoControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
 
+    @MockitoBean
+    private CargoService cargoService;
+
+    @Test
+    void cargoController_postCargos_ValidBody_ShouldReturn201Created() throws Exception {
+        CreateCargoRequest createCargoRequest = new CreateCargoRequest("123", 150, Location.CITY_B, Location.SEA_PORT_Z);
+
+        mockMvc.perform(post("/cargos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(createCargoRequest)
+                )
+        ).andExpect(status().isCreated());
+    }
+
+    @Test
+    void cargoController_postCargos_InvalidBody_TooLight_ShouldReturn400BadRequest() throws Exception {
+        CreateCargoRequest createCargoRequest = new CreateCargoRequest("123", 3, Location.CITY_B, Location.SEA_PORT_Z);
+
+        mockMvc.perform(post("/cargos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(createCargoRequest)
+                )
+        ).andExpect(status().isBadRequest());
+    }
 }
--- src/test/java/be/pxl/cargo/api/ShipmentControllerTest.java
+++ src/test/java/be/pxl/cargo/api/ShipmentControllerTest.java
@@ -1,7 +1,64 @@
 package be.pxl.cargo.api;
 
+import be.pxl.cargo.api.request.CreateCargoRequest;
+import be.pxl.cargo.domain.Location;
+import be.pxl.cargo.exceptions.ResourceNotFoundException;
+import be.pxl.cargo.service.CargoService;
+import be.pxl.cargo.service.ShipmentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ShipmentController.class)
 public class ShipmentControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private ShipmentService shipmentService;
+
+    @MockitoBean
+    private CargoService cargoService;
+
+    @BeforeEach
+    public void setup() {
+        CreateCargoRequest createCargoRequest = new CreateCargoRequest("123", 150, Location.CITY_B, Location.SEA_PORT_Z);
+
+        cargoService.createCargo(createCargoRequest);
+    }
+
+    @Test
+    void shipmentController_postShipments_addValidCargoToValidShipment_ShouldReturn200Ok() throws Exception {
+        mockMvc.perform(put("/shipments/1/cargo/123"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shipmentController_postShipments_addInvalidCargo_ShouldReturnNotFound() throws Exception {
+        doThrow(new ResourceNotFoundException())
+                .when(shipmentService)
+                .addCargoToShipment(900L, "invalid_cargo");
+
+        // Valid cargo works
+        mockMvc.perform(put("/shipments/1/cargo/123"))
+                .andExpect(status().isOk());
+
+        // Invalid throws
+        mockMvc.perform(put("/shipments/900/cargo/invalid_cargo"))
+                .andExpect(status().isNotFound());
+    }
 }
